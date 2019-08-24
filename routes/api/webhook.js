400e5f82b7a001d6bcb0639dc2b82eb13b429fc0
y-asai <ryuta234.you@gmail.com>
2019-08-24 15:01:42 +0900

    翻訳処理お試し

--- routes/api/webhook.js
+++ routes/api/webhook.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
 const line = require('@line/bot-sdk');
 
+//翻訳
+const request = require('request');
+
 require('dotenv').config({ silent: process.env.NODE_ENV === 'production' });
 
 const config = {
@@ -19,7 +22,42 @@ function handleEvent(event) {
   }
 
   // console.log('handleEvent', );
-  console.error('replyMessage = ', event.message.text);
+  // console.error('input' = ', event.message.text);
+  console.log('inputTxt = ', event.message.text);
+
+  /**
+   * 翻訳について
+   */
+  // 入力文字取得
+
+  // 文字チェック
+
+  // 翻訳するためのトークンを取得します。有効期限は取得後10分間です
+
+  // 翻訳するための文字列を生成します
+
+  // 実行
+  translateText = '';
+  getAccessToken(function (err, token) {
+    if (!err) {
+        // console.log(token);
+        translate(token, event.message.text, (err, translated) => {
+            if (!err)
+                console.log('TransLate == ', event.message.text, '->', translated);
+                translateText = translated;
+        });
+    }
+  });
+
+
+
+
+
+
+
+
+
+
   return client.replyMessage(event.replyToken, {
     type: 'text',
     text: event.message.text,
@@ -35,3 +73,70 @@ router.post('/', (req, res) => {
 });
 
 module.exports = router;
+
+/**
+ * 翻訳周り関連
+ */
+
+// アクセストークン取得
+function getAccessToken(callback) {
+  key = '351118560b1a45929f0d91492722b4af';
+  let headers = {
+      'Content-Type': 'application/json',
+      'Accept': 'application/jwt',
+      'Ocp-Apim-Subscription-Key': key
+  };
+  let options = {
+      url: 'https://api.cognitive.microsoft.com/sts/v1.0/issueToken',
+      method: 'POST',
+      headers: headers,
+      json: true
+  };
+
+  request(options, function (err, res) {
+      if (err) {
+          console.log(err);
+          callback(err, null);
+      } else
+          callback(null, res.body);
+  });
+}
+
+// 翻訳 (日本語 -> 英語)
+function translate(token, text, callback) {
+  let base_url = 'https://api.microsofttranslator.com/v2/http.svc/Translate',
+      appid = 'Bearer ' + token,
+      from = 'ja',
+      to = 'en';
+
+  let url = base_url + '?appid=' + appid + '&text=' + text + '&from=' + from + '&to=' + to;
+  let headers = {
+      'Accept': 'application/xml'
+  };
+
+  let options = {
+      url: encodeURI(url),
+      method: 'get',
+      headers: headers,
+      json: true
+  };
+
+  request(options, function (err, res) {
+      if (err) {
+          console.log(err);
+          callback(err, null);
+      } else
+          callback(null, res.body.replace(/<("[^"]*"|'[^']*'|[^'">])*>/g, ''));
+  });
+}
+
+// // 実行
+// getAccessToken(function (err, token) {
+//   if (!err) {
+//       // console.log(token);
+//       translate(token, process.argv[2], (err, translated) => {
+//           if (!err)
+//               console.log(process.argv[2], '->', translated);
+//       });
+//   }
+// });
